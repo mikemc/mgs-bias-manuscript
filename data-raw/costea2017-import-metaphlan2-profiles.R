f0da6e3eeb6291b6d89a220d08f321da8a924e7d
Michael McLaren <m.mclaren42@gmail.com>
2019-05-16 15:20:31 -0400

    Update metaphlan2 analysis of Costea2017 data
    Changes:
    - add bash safety options to shell scripts
    - remove need to manually edit paths in the shell script by assuming
      metaphlan2 is in the path and using the .env file to pass the data path
    - edit to pass number of processors as a commandline arg
    - add new script for re-running metaphlan2 on bowtie2 output with
      custom profiling options
    - edit import script and resave Rdata objects
    - update Costea analysis Rmd to use the new profiles
    
    The new profiles lower min_cu_len from 2000 to 0 and use the `avg_g`
    method instead of the `tavg_g` method for converting marker read counts
    to relative abundance estimates. These changes are aimed at increasing
    sensitivity and accuracy for the rarest mock taxa. Because all mock taxa
    are now detected in every sample, I removed the pseudocount-like step
    from the analysis. Other changes to the analysis are as follows: The new
    profiles introduced a new questionable identifier,
    `Shigella_unclassified`, which I now filter out. I also filter out
    `Escherichia_coli` and `Escherichia_unclassified`, since it is unclear
    if these reads are from the contaminant (which is mainly classified as
    `Shigella_flexneri`). The new profiles change the PCs and cause
    calibration to look less successful in the PCA plot on the original
    estimation set. I excluded the mock-only sample and picked a new
    estimation set, which gives more representative results of what I
    suspect a typical estimation set would give.

--- data-raw/costea2017-import-metaphlan2-profiles.R
+++ data-raw/costea2017-import-metaphlan2-profiles.R
@@ -1,28 +1,16 @@
-# library(tidyverse)
+library(tidyverse)
 
-## First we combine the individual sample profiles into a single profile
-
-# Fill in the paths to the metaphlan2 profiles and for the utility script
-# merge_metaphlan_tables.py included with metaphlan2 (in the "utils" folder)
-out_path = "/folder/for/metaphlan2/output"
-merge_metaphlan_path = "/path/to/merge_metaphlan_tables.py"
-
-command <- paste(
-    merge_metaphlan_path, 
-    paste0(out_path, "/*_profiled_metagenome.tsv"),
-    ">",
-    paste0(out_path, "/costea2017_metaphlan2_profiles.tsv")
-    )
-system(command)
-
-## Then, read into R and save for loading with the `data` function
-
-tb <- readr::read_tsv(paste0(out_path, "/costea2017_metaphlan2_profiles.tsv"))
+# Read in the merged metaphlan2 profiles
+dotenv::load_dot_env(here::here("data-raw", ".env"))
+profiles_fn <- file.path(Sys.getenv("DATA_PATH"), "costea2017", "metaphlan2",
+    "costea2017_metaphlan2_custom_profiles.tsv")
+tb <- read_tsv(profiles_fn)
 tb %>% glimpse
+# Improve column names
 tb <- tb %>% 
     rename(Clade = ID) %>%
     rename_at(vars(-Clade), ~str_extract(., "ERR[0-9]+"))
-tb %>% glimpse
-
+tb %>% names
+# Save for loading with the `data` function
 costea2017_metaphlan2_profiles <- tb
 usethis::use_data(costea2017_metaphlan2_profiles)
--- data-raw/costea2017-metaphlan2-custom.sh
+++ data-raw/costea2017-metaphlan2-custom.sh
@@ -11,23 +11,25 @@ nproc=$2
 
 # Set paths based on the DATA_PATH set in the .env file
 export $(cat .env | xargs)
-reads_path=$DATA_PATH/costea2017/reads
 out_path=$DATA_PATH/costea2017/metaphlan2
 
 metaphlan2.py \
-    --bowtie2out $out_path/${accession}_bowtie2.bz2 \
-    --nproc $nproc --input_type fastq \
-    $reads_path/${accession}_1.fastq.gz,$reads_path/${accession}_2.fastq.gz \
-    $out_path/${accession}_profiled_metagenome.tsv
+    --input_type bowtie2out \
+    --nproc $nproc \
+    --min_cu_len 0 \
+    --stat avg_g \
+    $out_path/${accession}_bowtie2.bz2 \
+    $out_path/${accession}_profiled_metagenome_custom.tsv
 
 # Submit all accessions to Slurm
 #
-# nproc=8
+# nproc=2
 # for i in {1971003..1971031}
 # do
 #     accession=ERR$i
-#     sbatch -c $nproc costea2017-metaphlan2.sh $accession $nproc
+#     # bash costea2017-metaphlan2-custom.sh $accession $nproc
+#     sbatch -c $nproc costea2017-metaphlan2-custom.sh $accession $nproc
 # done
 
 # Afterwards, merge all accessions into a single table with
-# /path/to/merge_metaphlan_tables.py *_profiled_metagenome.tsv > costea2017_metaphlan2_profiles.tsv
+# /path/to/merge_metaphlan_tables.py *_profiled_metagenome_custom.tsv > costea2017_metaphlan2_custom_profiles.tsv
